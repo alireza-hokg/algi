c9cd4dfc2a68faadf423268bd5e7eb40e01cd92f
pergina <alirezagolmakani9@gmail.com>
2026-05-05 15:38:37 +0330

    Start frontend

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,9 @@ import sequelize from "./utils/db.js";
 import productsRoutes from "./routes/products.js";
 import ProductVariantsRoutes from "./routes/product-variants.js";
 
+import "./models/products.js";
+import "./models/product-variants.js"
+
 const { SERVER_PORT = 9000 } = process.env;
 const app = express();
 
@@ -14,11 +17,14 @@ app.use(cors())
 app.use("/api/v1", productsRoutes)
 app.use("/api/v1", ProductVariantsRoutes)
 
+
 async function startServer() {
     try {
+        // Start syncing database
         await sequelize.sync({ force: true });
         console.log("Database connected successfully");
 
+        // Start listening to port 9000
         app.listen(SERVER_PORT, (req, res) => {
             console.log("Server is listening to port "+SERVER_PORT)
         })
@@ -28,8 +34,3 @@ async function startServer() {
 }
 
 startServer();
-
-
-// app.listen(SERVER_PORT, (req, res) => {
-//     console.log("server is listening to port "+SERVER_PORT)
-// })
---
+++ backend/models/users.js
@@ -0,0 +1,30 @@
+import { DataTypes } from "@sequelize/core";
+import sequelize from "../utils/db.js";
+
+const User = sequelize.define("user", {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false
+    },
+    firstname: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    lastname: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    phoneNumber: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    address: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    }
+}, {
+    timestamps: true
+})
+
+export default User;
