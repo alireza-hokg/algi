19313a520d2d54a7faaa55f0a633328d9f0b060a
pergina <alirezagolmakani9@gmail.com>
2026-05-06 22:40:36 +0330

    WOP users controller

---
+++ backend/controllers/users.js
@@ -0,0 +1,13 @@
+export default class UserController {
+    static async getUser(req, res) {
+        try {
+
+        } catch(err) {
+
+        }
+    }
+
+    static async createUser(req, res) {
+
+    }
+}
