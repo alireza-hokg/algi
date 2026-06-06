697efe2264351bd7a22af608678d239bf4d6e5b8
pergina <alirezagolmakani9@gmail.com>
2026-06-06 10:18:10 +0330

    add comment to Error.js

--- backend/utils/Error.js
+++ backend/utils/Error.js
@@ -1,12 +1,22 @@
 /**
- * For handling error message, statusCode and errorCode
+ * Custom error class for handling application errors with statusCode and errorCode
+ * 
  * @class
+ * @extends Error
  * 
  * @description
  * Creates a child class from Error class and adds statusCode and errorCode
+ * 
+ * @property {string} message - HTTP error message
+ * @property {string} errorCode - custom error identifiere (e.g 'NOT_FOUND')
+ * 
+ * @example
+ * throw new AppError("Not found", "404", "NOT_FOUND")
  */
 export class AppError extends Error {
     /**
+     * Creates an instance of AppError
+     * 
      * @param {string} message - Message caught from AppError
      * @param {number} statusCode - StatusCode caught from AppError
      * @param {string} errorCode - ErrorCode caught from AppError
@@ -17,12 +27,15 @@ export class AppError extends Error {
         this.errorCode = errorCode;
         this.isOperational = true;
 
-        // Error.captureStackTrace(this, this.contructor)
     }
 }
 /**
+ * error class for 404 not found responses.
+ * @class
+ * @extends AppError
+ * 
  * @description
- * Creates a child class from AppError class for 404 errors
+ * throws when requested resource (e.g page, product) can not be found.
  */
 export class NotFoundError extends AppError {
     constructor(message = "resource not found.") {
@@ -31,8 +44,13 @@ export class NotFoundError extends AppError {
 }
 
 /**
+ * error class for 400 validation 
+ * 
+ * @class
+ * @extends ValidationError
+ * 
  * @description
- * Creates a child class from AppError class when entry data in invalid
+ * throws when validation fails
  */
 export class ValidationError extends AppError {
     constructor(message = "Validation Failed") {
@@ -41,8 +59,11 @@ export class ValidationError extends AppError {
 }
 
 /**
+ * error class 500 database 
+ * @class
+ * @extends
  * @description
- * Creates a child class from AppError when server has problem
+ * throws when database fails to response
  */
 export class DatabaseError extends AppError {
     constructor(message = "Database error") {
@@ -51,8 +72,11 @@ export class DatabaseError extends AppError {
 }
 
 /**
+ * error class 409 conflict
+ * @class
+ * @extends AppError
  * @description
- * Creates a child class from AppError when have a conflict
+ * throws when there is a conflict between database and request
  */
 export class ConflictError extends AppError {
     constructor(message = "Conflict error") {
@@ -61,8 +85,12 @@ export class ConflictError extends AppError {
 }
 
 /**
+ * error class 401 authorization
+ * @class
+ * @extends AppError
+ * 
  * @description
- * Creates a child class from AppError when authorization is incorrect
+ * throws when authorization fails
  */
 export class UnauthorizedError extends AppError {
     constructor(message = "Unauthorized error") {
