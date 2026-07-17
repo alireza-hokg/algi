5fdc4d01da79370426e83becc966f2733f2a6061
pergina <alirezagolmakani9@gmail.com>
2026-07-17 10:35:50 +0330

    add order-item model and migration sequelize

---
+++ backend/migrations/20260717065643-create-order-item.cjs
@@ -0,0 +1,76 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Order-Items', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.BIGINT
+      },
+      orderId: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        allowNull: false,
+        references: {
+          model: 'Orders',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      productId: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        allowNull: false,
+        references: {
+          model: 'Products',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'RESTRICT'
+      },
+      quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+          min: 1
+        }
+      },
+      unitPrice: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false
+      },
+      totalPrice: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false
+      },
+      discount: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+        defaultValue: 0
+      },
+      finalPrice: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false
+      },
+      attributes: {
+        type: Sequelize.JSON,
+        allowNull: true,
+        comment: 'اطلاعات اضافی مثل رنگ، سایز و ...'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+      } 
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Order-Items');
+  }
+};
---
+++ backend/models/order-item.js
@@ -0,0 +1,69 @@
+import { Model } from "sequelize";
+
+export default (sequelize, DataTypes) => {
+  class OrderItem extends Model {
+
+  }
+
+  OrderItem.init({
+    id: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    orderId: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false,
+        references: {
+          model: 'Orders',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      productId: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false,
+        references: {
+          model: 'Products',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'RESTRICT'
+      },
+      quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          min: 1
+        }
+      },
+      unitPrice: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false
+      },
+      totalPrice: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false
+      },
+      discount: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+        defaultValue: 0
+      },
+      finalPrice: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false
+      },
+      attributes: {
+        type: DataTypes.JSON,
+        allowNull: true,
+        comment: 'اطلاعات اضافی مثل رنگ، سایز و ...'
+      },
+  }, {
+    timestamps: true,
+    sequelize,
+    modelName: "OrderItem"
+  })
+}
